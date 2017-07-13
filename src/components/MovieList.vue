851cd56c948d13e03ea5e39770b064f03fa4a500
Arielle Nguyen <arielle@ciedigital.com>
2017-07-13 16:53:24 -0700

    Add working filters and single file components

---
+++ src/components/MovieList.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div id="movie-list">
+    <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
+  </div>
+</template>
+
+<script>
+  import genres from '../util/genres.js';
+  export default {
+  data() {
+    return {
+      movies: [
+        { title: 'Pulp Fiction', genre: genres.CRIME },
+        { title: 'Home Alone', genre: genres.COMEDY },
+        { title: 'Austin Powers', genre: genres.COMEDY }
+      ]
+    };
+  },
+  props: ['genre', 'time'],
+  methods: {
+    moviePassesGenresFilter(movie) {
+      if (!this.genre.length) return true;
+      this.genre.find(genre => movie.genre === genre);
+    }
+  },
+  computed: {
+    filteredMovies() {
+      //return this.movies.filter(this.moviePassesGenresFilter);
+      if (!this.genre.length) return this.movies;
+      return this.movies.filter(movie => this.genre.find(genre => movie.genre === genre));
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import './style.scss';
+
+import genres from './util/genres.js';
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+new Vue({
+  el: '#app',
+  data: {
+    genre: [],
+    time: []
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (this[category].includes(title)) {
+          this[category].splice(index, 1);
+        }
+      }
+    }
+  },
+  components: {
+    MovieList,
+    MovieFilter
+  }
+});
